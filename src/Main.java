9d7fa5bea276b955c5e2884cb1199f3a8e07a13c
Vitaliy-Avdeev <avd.vit78@gmail.com>
2024-10-06 17:20:48 +0300

    Message changed to "Продуктов в одной машине 22 кг"

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
      System.out.println(box);
      box = box - 3;
      System.out.println(box);
+     box = box * 3;
+     System.out.println(box);
+     box = box / 4;
+     System.out.println(box);
 
      var liftingCapacity = 50;
      var stuffWeight = 20;
@@ -15,5 +19,31 @@ public class Main {
      var orangesWeight = 3;
      var fruitWeight = appleWeight + orangesWeight;
      System.out.println("Общий вес фруктов " + fruitWeight);
+
+     var meatWeight = 4;
+     var waterWeight = 5;
+     var tomatoWeight = 2;
+     var cucumbersWeight = 2;
+     var peppersWeight = 2;
+     var zucchiniWeight = 2;
+     var vegetablesWeight = tomatoWeight + cucumbersWeight + peppersWeight +  zucchiniWeight;
+     var productsWeight = fruitWeight + vegetablesWeight + meatWeight + waterWeight;
+     System.out.println("Общий вес продуктов " + productsWeight + " кг!");
+
+     var leftWeight = liftingCapacity - stuffWeight - productsWeight;
+     System.out.println("Места осталось " + leftWeight + " кг!");
+
+     productsWeight = productsWeight * 2;
+     System.out.println("Теперь вес продуктов " + productsWeight + " кг!");
+
+     leftWeight = liftingCapacity - stuffWeight - productsWeight;
+     System.out.println("Теперь места осталось " + leftWeight + " кг!");
+
+     var overload = (stuffWeight + productsWeight) % liftingCapacity;
+     System.out.println("Перегруз на " + overload + " кг!");
+
+     var productsInOneCar = productsWeight / 2;
+     System.out.println("Продуктов в одной машине " + productsInOneCar + " кг!");
+
             }
         }
